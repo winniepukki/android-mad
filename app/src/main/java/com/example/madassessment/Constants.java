1009dbfda5621d5cd0ed5e3c612162cc702f7121
Aleksandrs Bogackins <4bogaa25@solent.ac.uk>
2020-03-08 19:13:36 +0000

    ref: project structure, imp: initial version

---
+++ app/src/main/java/com/example/madassessment/Constants.java
@@ -0,0 +1,14 @@
+package com.example.madassessment;
+
+public class Constants {
+    /*
+    *   Setting the default coordinates
+    *   for Helsinki, Finland. Zoom to 12
+    */
+    public static final Double DEFAULT_LAT = 60.169;
+    public static final Double DEFAULT_LON = 24.938;
+    public static final Integer DEFAULT_ZOOM = 12;
+
+    public static final String  NORMAL_MAP = "normal";
+    public static final String  CYCLE_MAP = "cycle";
+}
--- app/src/main/java/com/example/madassessment/MainActivity.java
+++ app/src/main/java/com/example/madassessment/MainActivity.java
@@ -1,13 +1,93 @@
 package com.example.madassessment;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 
+import android.location.LocationManager;
+import android.location.LocationListener;
+import android.location.Location;
+import android.content.Context;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    MapView mv;
+    
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        LocationManager mgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = mgr.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        double latitude = 60.169;
+        double longitude = 24.938;
+
+        if (location != null) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+        }
+
+        // This line sets the user agent, a requirement to download OSM maps
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+
         setContentView(R.layout.activity_main);
+
+        mv = findViewById(R.id.map1);
+
+        mv.setMultiTouchControls(true);
+        mv.getController().setZoom(16.0);
+        mv.getController().setCenter(new GeoPoint(latitude, longitude));
     }
+
+    /*
+     *   Handling changes on menu selection
+     */
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.choosemap) {
+            //Intent intent = new Intent(this, .class);
+            //startActivity(intent);
+            return true;
+        }
+        return false;
+    }
+
+    /*
+     *   Handling changes in location preferences
+     */
+    public void onLocationChanged(Location newLoc) {
+        Toast.makeText(this, "Location=" + newLoc.getLatitude() + " " + newLoc.getLongitude(), Toast.LENGTH_LONG).show();
+        mv.getController().setCenter(new GeoPoint(newLoc.getLatitude(), newLoc.getLongitude()));
+    }
+
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Provider " + provider + " disabled", Toast.LENGTH_LONG).show();
+    }
+
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Provider " + provider + " enabled", Toast.LENGTH_LONG).show();
+    }
+
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Toast.makeText(this, "Status changed: " + status, Toast.LENGTH_LONG).show();
+    }
+
 }
+
